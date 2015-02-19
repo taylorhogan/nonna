7d431d7b2678dcd016ce2d7cd6803b11de4d6599
taylor <taylorhogan@me.com>
2015-02-19 07:11:15 -0500

    no message

---
+++ mail-recent.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+tail -1 temp.csv > mail.txt
+tail -1 motion.csv >> mail.txt
+mail -s "subject" taylorhogan@me.com  < mail.txt
+
--- pull-new.sh
+++ pull-new.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # git clone https://github.com/taylorhogan/nonna nonna
 # get the latest from git
+
+rm pull-new.sh
+
 git pull
+
+chmod +x pull-new.sh
 
 #install the new crontab
 crontab crontab.txt
+
 #this should come next
+#again
 
 
 
